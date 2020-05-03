d5e5e7f693eeebabeda747f262fd950f7cc19294
Yevhenii Malyshko <yevheniimalushko@zoho.com>
2020-05-03 11:07:44 +0300

    Added button scroll-up

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     }
   });
 });
+$(document).ready(function(){
+	$(window).scroll(function(){
+	if ($(this).scrollTop() > 250) {
+	$('.scrollup').fadeIn();
+	} else {
+	$('.scrollup').fadeOut();
+	}
+});
+$('.scrollup').click(function(){
+$("html, body").animate({ scrollTop: 0 }, 800);
+return false;
+});
+ 
+});
