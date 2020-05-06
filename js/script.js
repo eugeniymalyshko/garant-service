4bebac66e7e887665dd5c2eda7e6888e7acf29b8
Yevhenii Malyshko <yevheniimalushko@zoho.com>
2020-05-07 01:07:44 +0300

    Capion now hide in push burger menu

--- js/script.js
+++ js/script.js
@@ -7,18 +7,47 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
       $("#navbarSupportedContent").collapse('hide');
     }
   });
+
+  // In Firefox and Safari, the click event doesn't retain the focus
+  // on the clicked button. Therefore, the blur event will not fire on
+  // user clicking somewhere else in the page and the blur event handler
+  // which is set up above will not be called.
+  // Refer to issue #28 in the repo.
+  // Solution: force focus on the element that the click event fired on
+  $("#navbar-toggler").click(function (event) {
+    $(event.target).focus();
+  });
 });
-$(document).ready(function(){
-	$(window).scroll(function(){
-	if ($(this).scrollTop() > 250) {
-	$('.scrollup').fadeIn();
-	} else {
-	$('.scrollup').fadeOut();
-	}
+
+
+
+
+$(function(){
+    $(".navbar-toggler").click(function(){ // задаем функцию при нажатиии на элемент с классом out
+      $( ".caption" ).fadeToggle(); // плавно изменяя прозрачность скрываем все элементы <div>
+    });
+  });
+
+
+//** ScrollUp Hide/Show **
+$(function() {
+  //при нажатии на кнопку scrollup
+  $('.scrollup').click(function() {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop: 0
+    },1000);
+  })
 });
-$('.scrollup').click(function(){
-$("html, body").animate({ scrollTop: 0 }, 800);
-return false;
-});
- 
+// при прокрутке окна (window)
+$(window).scroll(function() {
+  // если пользователь прокрутил страницу более чем на 200px
+  if ($(this).scrollTop()>200) {
+    // то сделать кнопку scrollup видимой
+    $('.scrollup').fadeIn();
+  }
+  // иначе скрыть кнопку scrollup
+  else {
+    $('.scrollup').fadeOut();
+  }
 });
